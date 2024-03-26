a283d6d80fd980b4f35b04875a2d3d90669fdb0f
Marchopkins96 <marchopkins1@outlook.com>
2024-03-26 15:37:47 +0000

    Update code in settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v0qe2we@6kyxzcov*3mhz)lzkzk$(*h9c4)u-k((!7z0a^b$*y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-marchopkins96-django-pro-l2vr65w6ju.us1.codeanyapp.com'
 ,'.herokuapp.com']
